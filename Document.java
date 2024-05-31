783d6852bcc0fb244986a9c6c2879a1aebe37b33
monu ghorela <monughorela2002@gmail.com>
2024-05-31 07:49:54 +0530

    second commit

--- Document.java
+++ Document.java
@@ -1,5 +1,8 @@
 class Document{
     public static void main(String[] args) {
         System.out.println("java filoe");
+        System.out.println("java file2");
+        System.out.println("hello world");
+
     }
 }
