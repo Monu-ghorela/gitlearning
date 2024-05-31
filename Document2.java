cec7803bfc72aa64ec5dac0a2a09d4cbd315aca7
monu ghorela <monughorela2002@gmail.com>
2024-05-31 08:16:05 +0530

    this is the first commit

---
+++ Document2.java
@@ -0,0 +1,11 @@
+/**
+ * Document2
+ */
+public class Document2 {
+
+    public static void main(String[] args) {
+        System.out.println("this is another file  i have used to push in github");
+        System.out.println("hello wordld");
+        System.out.println("this is the message from master monu");
+    }
+}
